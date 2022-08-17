516181d25bcdcfcf8bb68152a28cfe7187f4fbdd
Shahrad Elahi <shahradq@gmail.com>
2022-08-18 00:33:49 +0430

    Added `.env` for template

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -8,22 +8,8 @@ use TelegramBot\Request;
 class RequestTest extends \PHPUnit\Framework\TestCase
 {
 
-    private function load_env()
-    {
-        $env = file_get_contents(__DIR__ . '/../.env');
-        $env = explode("\n", $env);
-        foreach ($env as $line) {
-            $line = explode('=', $line);
-            if (count($line) == 2) {
-                putenv($line[0] . '=' . $line[1]);
-            }
-        }
-    }
-
     public function test_send_message()
     {
-        $this->load_env();
-
         $result = Request::create('sendMessage', [
             'chat_id' => '259760855',
             'text' => 'text',
