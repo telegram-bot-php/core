a0414b339c84a12a43b3e7e96e3c328113566ca5
Shahrad Elahi <shahradq@gmail.com>
2024-06-24 08:43:23 +0330

    fix

--- tests/Examples/EchoBot/Plugins/MainPlugin.php
+++ tests/Examples/EchoBot/Plugins/MainPlugin.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace TelegramBotTest\Examples\EchoBot\Plugins;
 
+use PHPUnit\Framework\Assert;
 use TelegramBot\Entities\Message;
 
 class MainPlugin extends \TelegramBot\Plugin {
 
-    public function onMessage(int $updateId, Message $message): \Generator {
-        echo "UpdateId: " . $updateId . ", Text: " . $message->getText();
+    public function onMessage(int $update_id, Message $message): \Generator {
+        Assert::assertEquals('Hello World!', $message->getText());
+        Assert::assertEquals(1, $update_id);
         yield;
     }
 
